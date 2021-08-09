04640d2f0571acc17c4a57e60a2ae85a29939a0c
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-08-08 23:19:26 -0300

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, withSpring, withRepeat } from 'react-native-reanimated';
+
+const SIZE = 100.0;
+
+// const handleRotation = (progress: Animated.SharedValue<number>) => {
+//   return `${progress.value * 2 * Math.PI}rad`;
+// }
 
 export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(1);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [{ scale: scale.value }, { rotate: `${progress.value * 2 * Math.PI}rad`},
+    ]
+    }
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), -1, true);
+    scale.value = withRepeat(withSpring(1), 3, true);
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[{height: SIZE, width: SIZE, backgroundColor: 'purple'}, reanimatedStyle]} />
     </View>
   );
 }
